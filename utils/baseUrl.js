1f096bab4f69b759da231032decdc56f28551192
erkabyte <free3erkaa@yahoo.com>
2023-04-11 21:44:44 +0800

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "http://pii-7ya2-py4rogsth-eggy011235.vercel.app"
+		? "https://pii-7ya2.vercel.app"
 		: "http://localhost:3000";
 
 export default baseUrl;
